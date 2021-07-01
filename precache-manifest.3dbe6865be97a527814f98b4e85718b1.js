47733a53d4cdfa5ca046e8f2137a76816538d19d
maxbarsukov <maximbarsukov@bk.ru>
2021-07-01 14:01:39 +0300

    Updates

---
+++ precache-manifest.3dbe6865be97a527814f98b4e85718b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "44204fabb877fef8fa24",
+    "url": "/static/js/main.44204fab.chunk.js"
+  },
+  {
+    "revision": "b2231659268079ae2f43",
+    "url": "/static/js/1.b2231659.chunk.js"
+  },
+  {
+    "revision": "a6564de42e4c19309c78",
+    "url": "/static/js/2.a6564de4.chunk.js"
+  },
+  {
+    "revision": "0646364dd12bc9d0babc",
+    "url": "/static/js/3.0646364d.chunk.js"
+  },
+  {
+    "revision": "87e1671cb5bb4a87c117",
+    "url": "/static/js/4.87e1671c.chunk.js"
+  },
+  {
+    "revision": "a61494208f5a83e9fded",
+    "url": "/static/js/runtime~main.a6149420.js"
+  },
+  {
+    "revision": "ff68f19c2097d175682361f81d0bf1ca",
+    "url": "/index.html"
+  }
+];
