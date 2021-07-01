41783381a1a880beaf20986bf4bb400715179acc
maxbarsukov <maximbarsukov@bk.ru>
2021-07-01 13:54:33 +0300

    Global Update

---
+++ README.md
@@ -0,0 +1,3 @@
+# Resume
+
+[**TODO-do-do-do-to-dodododoooo**](https://www.youtube.com/watch?v=NeNtRWaPT38)
---
+++ src/pages/home/home-data.ts
@@ -0,0 +1,8 @@
+export default {
+  title: 'Max Barsukov',
+  subTitle: 'Junior Ruby on Rails developer',
+  descriptions: [
+    "A  self-taught 16 years old developer with over 0 years of commercial experience in various technologies including Ruby on Rails, ReactJS, PostgreSQL that are part of products used by only a handful of people around the globe",
+  ],
+  profilePicUrl: 'https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/-eCarCr34xPGrg',
+}
