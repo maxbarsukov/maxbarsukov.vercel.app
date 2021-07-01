28becbc62168e68c4eace02b5f492abbea12d15b
maxbarsukov <maximbarsukov@bk.ru>
2021-07-01 14:18:26 +0300

    Images loaded

--- src/pages/home/home-data.ts
+++ src/pages/home/home-data.ts
@@ -4,5 +4,5 @@ export default {
   descriptions: [
     "A  self-taught 16 years old developer with over 0 years of commercial experience in various technologies including Ruby on Rails, ReactJS, PostgreSQL that are part of products used by only a handful of people around the globe",
   ],
-  profilePicUrl: 'https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/d/-eCarCr34xPGrg',
+  profilePicUrl: '/img/me.jpg',
 }
