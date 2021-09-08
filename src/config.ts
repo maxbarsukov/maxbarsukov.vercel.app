08d628e5e1c4cc432492ffac714736b92ea7ffea
maxbarsukov <maximbarsukov@bk.ru>
2021-09-08 18:03:10 +0300

    Some themes added

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,13 @@
+import _ from "lodash";
 import { theme } from "@chakra-ui/react"
 
 enum Theme {
     DESERT = "desert",
     AURORA = "aurora",
-    ABSTRACT = "abstract"
+    ABSTRACT = "abstract",
+    CYBER = "cyber",
+    SKY = "sky",
+    PINKSKY = "pinksky"
 }
 
 export const themeConfig = {
@@ -21,9 +25,24 @@ export const themeConfig = {
         color: "purple",
         main: theme.colors.purple[600],
         darker: theme.colors.purple[800],
+    },
+    [Theme.CYBER]: {
+        color: "pink",
+        main: theme.colors.purple[800],
+        darker: theme.colors.teal[600],
+    },
+    [Theme.SKY]: {
+        color: "blue",
+        main: theme.colors.orange[700],
+        darker: theme.colors.black[700],
+    },
+    [Theme.PINKSKY]: {
+        color: "pink",
+        main: theme.colors.pink[500],
+        darker: theme.colors.pink[900],
     }
 }
 
 export const config = {
-    theme: Theme.ABSTRACT
+    theme: _.sample(Object.values(Theme)) as Theme
 }
